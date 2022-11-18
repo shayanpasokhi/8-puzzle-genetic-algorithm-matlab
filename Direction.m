0bccaa5012c1b59d69651c1e56e820ed669f1e55
Shayan Mohammad Pasokhi Razavi <shayanrazavi33@yahoo.com>
2022-11-18 22:41:47 +0330

    add base direction

--- Direction.m
+++ Direction.m
@@ -1,34 +1,59 @@
 classdef Direction < int8
     enumeration
-      Up (1)
-      Right (2)
-      Down (3)
-      Left (4)
+      base (0)
+      up (1)
+      right (2)
+      down (3)
+      left (4)
     end
+
     methods
+      function str = getDirectionStr(obj)
+        tmp = ['U', 'R', 'D', 'L'];
+        str = tmp(obj);
+      end
+          
       function is = isEqual(obj, direction)
         is = obj == direction;
       end
+
       function is = isOpposite(obj, direction)
-        is = abs(obj - direction) == 2;
+        if obj == 0 || direction == 0
+          is = 0;
+        else
+          is = abs(obj - direction) == 2;
+        end
       end
+
       function opp = getOpposite(obj)
-        if obj > 2
+        if obj == 0
+          opp = Direction(0);
+        elseif obj > 2
           opp = Direction(obj - 2);
         else
           opp = Direction(obj + 2);
         end
       end
+
       function diff = getDifferent(obj)
-        i = [1 2 3 4];
-        i(i == obj) = [];
-        diff = Direction(i(randi(3)));
+        if obj == 0
+          diff = Direction(0);
+        else
+          i = [1, 2, 3, 4];
+          i(i == obj) = [];
+          diff = Direction(i(randi(3)));
+        end
       end
+
       function diff = getDifferentAxis(obj)
-        i = [1 2 3 4];
-        i(i == obj) = [];
-        i(i == obj.getOpposite()) = [];
-        diff = Direction(i(randi(2)));
+        if obj == 0
+          diff = Direction(0);
+        else
+          i = [1, 2, 3, 4];
+          i(i == obj) = [];
+          i(i == obj.getOpposite()) = [];
+          diff = Direction(i(randi(2)));
+        end
       end
     end
 end
